99e1b773f950ba1594488a053be48d7c5427b87d
shrutikamati <kamatishruti@gmail.com>
2016-08-18 22:30:34 -0700

    commiting example

--- java_ex1.java
+++ java_ex1.java
@@ -4,8 +4,8 @@ public class TheMotivator {
 						System.out.println("You're awesome");
 								else if (score > 90)
 											System.out.println("That's great");
-													else if (score > 60)
-																System.out.println("That's good ");
+			else if (score > 60)
+			System.out.println("That's good ");
 																		else
 																					System.out.println("Well, what can I say?");
 																						}
